f6805e6ea7dea2219dffd3cc6975f19167581d95
zyllar68 <zyrilmancion@gmail.com>
2022-09-14 00:58:16 +0800

    login/register wrapper update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CardComponent } from './components/card/card.component';
+import { LoginSignupWrapperComponent } from './components/login-signup-wrapper/login-signup-wrapper.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroBannerComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    CardComponent,
+    LoginSignupWrapperComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

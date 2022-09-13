4243276cf31bab046387b159cc13210c26d4bd3a
zyllar68 <zyrilmancion@gmail.com>
2022-09-13 14:14:42 +0800

    navbar done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  burgerActive = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onToggleBurger() {
+    this.burgerActive = !this.burgerActive;
+  }
 }

80920b069d6d4a9867b2c7acc8f4c89d61a97454
zyllar68 <zyrilmancion@gmail.com>
2022-09-14 17:28:27 +0800

    login page done

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() label: any;
+  @Input() icon: any;
+  @Input() errorMessage: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
